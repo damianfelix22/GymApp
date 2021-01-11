35a1278834e1bc0d321126b7ea5d60fe056ee6e2
Damian Felix <damianfelix22@gmail.com>
2021-01-11 20:48:15 -0300

    Barra de búsqueda
    se agregó una barra de búsqueda para buscar una clase por su nombre o por su instructor

--- src/app/modules/clases/clases.page.ts
+++ src/app/modules/clases/clases.page.ts
@@ -13,6 +13,8 @@ export class ClasesPage implements OnInit {
     user_role: 'admin'
   }
 
+  miBuscador: any;
+
   clases: any[] = [
     {
       name: 'Taekwondo',
@@ -27,7 +29,7 @@ export class ClasesPage implements OnInit {
       name: 'Spinning',
       start: '14:00',
       end: '16:00',
-      instructor: 'Pablo',
+      instructor: 'Roberto',
       image: 'https://www.hola.com/imagenes/estar-bien/20190206136791/ventajas-inconvenientes-spinning-cs/0-643-792/proscontraspinning-t.jpg',
       description: `Lorem ipsum dolor sit amet consectetur adipisicing elit.`,
     },
@@ -35,16 +37,41 @@ export class ClasesPage implements OnInit {
       name: 'Crossfit',
       start: '14:00',
       end: '16:00',
-      instructor: 'Pablo',
+      instructor: 'Maria',
       image: 'https://www.wellandgood.com/wp-content/uploads/2018/11/Stocksy-crossfit-friends-BONNINSTUDIO.jpg',
       description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi assumenda a 
       dignissimos nobis voluptatibus obcaecati sit explicabo, eius voluptatum.`,
     },
-  ];
+  ]
+
+  clasesEncontradas: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.clasesEncontradas = this.clases;
+  }
+
+  buscar(ev: any) {
+    this.clasesEncontradas = [];
+    let clase = "";
+    let instructor = "";
+    //console.log(text.indexOf(ev.srcElement.value));
+    for (let i = 0; i < this.clases.length; i++){
+      clase = this.clases[i].name.toLowerCase();
+      if (clase.indexOf(ev.srcElement.value.toLowerCase()) != -1)
+        this.clasesEncontradas.push(this.clases[i]);
+    }
+
+    for (let i = 0; i < this.clases.length; i++){
+      instructor = this.clases[i].instructor.toLowerCase();
+      if (instructor.indexOf(ev.srcElement.value.toLowerCase()) != -1)
+        this.clasesEncontradas.push(this.clases[i]);
+    }
+  }
+
+  cancelarBusqueda() {
+    this.clasesEncontradas = this.clases;
   }
 
 }
