0eedb5d1728df21f5c4b50949eb86256035a6ae5
Damian Felix <damianfelix22@gmail.com>
2021-03-05 17:21:25 -0300

    layout

--- src/app/modules/gym/gym.page.ts
+++ src/app/modules/gym/gym.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-gym',
@@ -21,16 +22,27 @@ export class GymPage implements OnInit {
   };
   clases: any;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
     this.clases = this.gym.classes;
 
     this.gymForm = new FormGroup({
-      name: new FormControl(this.gym.name, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
-      lastname: new FormControl(this.gym.lastname, [Validators.required, Validators.pattern('[a-zA-Z ]*')]),
+      name: new FormControl(this.gym.name, [Validators.required, Validators.pattern('[a-zA-Z 0-9]*')]),
       tel: new FormControl(this.gym.tel, [Validators.required, Validators.pattern('[+ 0-9 ]*')]),
     });
+  }
+
+  onSubmit() {
+    this.router.navigateByUrl('menu');
+  }
+
+  atras(){
+    if(this.gymForm.valid){
+      this.router.navigateByUrl('menu');
+    }else{
+      alert('Revisa los datos requeridos')
+    }
   }
 
 }
